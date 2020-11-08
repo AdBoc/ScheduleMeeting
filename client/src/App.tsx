2142a757ac41e1463a87e2605214d2f0aefea271
AdBoc <adrianboczon98@gmail.com>
2020-11-08 09:44:00 +0100

    fixed typescript error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,7 @@ import "./assets/normalize.css";
 
 function App() {
   return (
+    //@ts-ignore
     <HashRouter history={history}>
       <Switch>
         <Route exact path="/" component={CalendarPage}/>
